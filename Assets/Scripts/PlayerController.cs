4d0b0888d6d3ecbf6eaaacce92bda457dec7d05c
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-07-27 13:29:07 +0200

    kind of the first commit, something went wrong with the first try of making a git repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigidbody;
+    private BoxCollider boxcollider;
+    private bool Player_1 = false;
+    private bool Player_2 = false;
+    public GameObject Player;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        boxcollider = transform.GetComponent<BoxCollider>();
+        if (Player.tag == "Player_1"){
+            Player_1 = true;
+        }
+        else if (Player.tag == "Player_2"){
+            Player_2 = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Player_1 == true){
+            float moveHorizontalP1 = Input.GetAxis("P1Horizontal");
+            float moveVerticalP1 = Input.GetAxis("P1Vertical");
+            Vector3 moveP1 = new Vector3(moveHorizontalP1, moveVerticalP1, 0.0f);
+            rigidbody.velocity = moveP1 * speed;
+        }
+        else if (Player_2 == true){
+            float moveHorizontalP2 = Input.GetAxis("P2Horizontal");        
+            float moveVerticalP2 = Input.GetAxis("P2Vertical");
+            Vector3 moveP2 = new Vector3(moveHorizontalP2, moveVerticalP2, 0.0f);
+            rigidbody.velocity = moveP2 * speed;
+        }
+        
+
+        
+       
+
+    }
+}
