18e0fbc34e71f8fa56cb34d77d2d4c58ba66fbb7
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-08-04 22:32:07 +0200

    added a ball, it moves in a random direction. Also changed a variable name

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private Rigidbody rigidbody;
+    private Rigidbody rb;
     private BoxCollider boxcollider;
     private bool Player_1 = false;
     private bool Player_2 = false;
     public GameObject Player;
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         boxcollider = transform.GetComponent<BoxCollider>();
         if (Player.tag == "Player_1"){
             Player_1 = true;
         }
         else if (Player.tag == "Player_2"){
             Player_2 = true;
-        }
+        } 
     }
 
     // Update is called once per frame
@@ -29,13 +29,13 @@ public class PlayerController : MonoBehaviour
             float moveHorizontalP1 = Input.GetAxis("P1Horizontal");
             float moveVerticalP1 = Input.GetAxis("P1Vertical");
             Vector3 moveP1 = new Vector3(moveHorizontalP1, moveVerticalP1, 0.0f);
-            rigidbody.velocity = moveP1 * speed;
+            rb.velocity = moveP1 * speed;
         }
         else if (Player_2 == true){
             float moveHorizontalP2 = Input.GetAxis("P2Horizontal");        
             float moveVerticalP2 = Input.GetAxis("P2Vertical");
             Vector3 moveP2 = new Vector3(moveHorizontalP2, moveVerticalP2, 0.0f);
-            rigidbody.velocity = moveP2 * speed;
+            rb.velocity = moveP2 * speed;
         }
         
 
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        move = new Vector3 (Random.Range(-1,1), Random.Range(-1,1), Random.Range(-1,1));
+        Debug.Log(move);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rb.velocity = move * speed;
+    }
+}
